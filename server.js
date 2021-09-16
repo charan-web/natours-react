e79eb45151332d628b277f14b2f05322c8ee117f
cc710788@gmail.com <cc710788@gmail.com>
2021-09-16 18:55:39 +0530

    changes-3

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ if(process.env.NODE_ENV = "production"){
     })
 }           
  
+ 
 
-
-const port =  process.env.PORT ||8080     
+const port =  process.env.PORT || 8080     
        
 app.listen(port,()=>{
     console.log(`app is listening on ${port}`)
