6a5ffef3ed7ae7b56a48883b2a27f6477b79c18a
cc710788@gmail.com <cc710788@gmail.com>
2021-11-03 18:44:50 +0530

    last

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -67,7 +67,7 @@ exports.bookingSession= catchAsync(async (req,res,next)=>{
     // await Booking.create({tour,user,price})
 
     // res.redirect(req.originalUrl.split('?')[0])
-    const signature = req.headers['stripe-signature'].replace('[',' ')
+    const signature = req.headers['stripe-signature'].replace('[',' ').repalce(']'," ").trim()
     let event 
     try {
         event = stripe.webhooks.constructEvent(
