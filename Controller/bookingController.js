e5e817acf06ef2521093b03ec7969e23ad0585a7
cc710788@gmail.com <cc710788@gmail.com>
2021-11-03 18:32:16 +0530

    last

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -24,7 +24,7 @@ exports.getCheckoutSession = catchAsync(async(req,res,next)=>{
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types:['card'],
-        success_url:`${req.protocol}://${req.get('host')}/my-tours`,
+        success_url:`https://dreamy-shaw-9ac27c.netlify.app/booking/my-tours`,
         cancel_url:`${req.protocol}://${req.get('host')}/`,
         customer_email:req.user.email,
         client_reference_id:req.params.tourid,
