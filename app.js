e532bc1848a27deb5e800026af01feb4f34a708a
cc710788@gmail.com <cc710788@gmail.com>
2021-11-03 09:51:26 +0530

    last

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
-
+const path = require('path')
 const cors = require('cors')
 const compression = require('compression')
 const bookingController = require('./Controller/bookingController')
