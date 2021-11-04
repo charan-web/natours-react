27f0828f9f15bb694daf47c9c24c11f067200f05
cc710788@gmail.com <cc710788@gmail.com>
2021-11-04 11:18:09 +0530

    last

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -53,7 +53,7 @@ exports.bookingCheckout=async (session)=>{
     try{
         const tour = session.client_reference_id 
         const user = (await User.findOne({email:session.customer_email})).id
-        const price = session.list_items[0].amount / 100 
+        const price = session.line_items[0].amount / 100 
         await Booking.create({tour,user,price})
         console.log("created a book tour")
        
